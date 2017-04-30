6fffc651f4ddffad1aae1de04622d178b33e392f
Mahima-95 <agg.mahima@gmail.com>
2017-04-30 22:11:35 +0530

    hard coded request removed..

--- src/main/java/com/hibernate/library/controller/LibraryController.java
+++ src/main/java/com/hibernate/library/controller/LibraryController.java
@@ -3,8 +3,11 @@ package com.hibernate.library.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hibernate.library.entity.Library;
@@ -15,32 +18,34 @@ public class LibraryController {
 	@Autowired
 	private LibraryService libraryService;
 
-	@RequestMapping("/addBook")
-	public String addBook() {
-		Library library = new Library();
-		library.setBookName("Mahima");
-		library.setBookAuthor("chetan bhagat");
+	@RequestMapping(value = "/addBook", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public String addBook(@RequestBody Library library) {
+		// Library library = new Library();
+		// library.setBookName("Mahima");
+		// library.setBookAuthor("chetan bhagat");
 		libraryService.addBook(library);
 		return "Successfully inserted..!!!";
 	}
-	
-	@RequestMapping("/getBook")
-	public List<Library> getBook(){
+
+	@RequestMapping(value = "/getBook", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public List<Library> getBook() {
 		return libraryService.getBook();
-		
+
 	}
-	@RequestMapping("/deleteBook/{bId}")
-	public String deleteBook(@PathVariable int bId){
-		Library library = new Library();
+
+	@RequestMapping(value = "/deleteBook/{bId}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public String deleteBook(@PathVariable int bId) {
+		// Library library = new Library();
 		libraryService.deleteBook(bId);
 		return "Deleted..!!!";
-		
+
 	}
-	@RequestMapping("/updateBook")
-	public String updateBook(){
-		Library library = new Library();
-		library.setBookId(2);
-		library.setBookName("five point someone");
+
+	@RequestMapping(value = "/updateBook", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public String updateBook(@RequestBody Library library) {
+		// Library library = new Library();
+		// library.setBookId(2);
+		// library.setBookName("five point someone");
 		return libraryService.updateBook(library);
 	}
 
